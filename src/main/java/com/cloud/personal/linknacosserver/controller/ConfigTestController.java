1c7206b93606df5921b6af5830892161b5c44084
刘立俊 <645604899@qq.com>
2020-06-14 11:42:45 +0800

    nacos集群配置

--- src/main/java/com/cloud/personal/linknacosserver/controller/ConfigTestController.java
+++ src/main/java/com/cloud/personal/linknacosserver/controller/ConfigTestController.java
@@ -2,10 +2,12 @@ package com.cloud.personal.linknacosserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @Slf4j
 public class ConfigTestController {
 
